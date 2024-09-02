bb7ca5cef8591907bb4336c61dc645af803b6029
MoDerFEe <159480211+MoDerFEe@users.noreply.github.com>
2024-09-02 17:31:24 +1100

    Add files via upload

---
+++ home_ex_2.py
@@ -0,0 +1,5 @@
+count_hw = 12
+count_h = 1.5
+course_name = 'Python'
+time_ex = count_h/count_hw
+print(course_name, ', всего задач: ',count_hw, ', затрачено часов: ',count_h, ', среднее время выполнения ',time_ex,' часа.')
