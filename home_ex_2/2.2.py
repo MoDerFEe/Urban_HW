a249aba3fc3a15103f3d4e2f14a20c55afec02ba
MoDerFEe <159480211+MoDerFEe@users.noreply.github.com>
2024-09-09 00:12:03 +1100

    Add files via upload

---
+++ home_ex_2/2.2.py
@@ -0,0 +1,14 @@
+try:
+    a=int(input())
+    b=int(input())
+    c=int(input())
+
+    if a==b and a==c and b==c:
+        print(3)
+    elif a!=b and a!=c and b!=c:
+        print(0)
+    else:
+        print(2)
+        
+except:
+    False
