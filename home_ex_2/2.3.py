2604f22ba400ebdcbf1003fe572e5c7e212903e5
MoDerFEe <159480211+MoDerFEe@users.noreply.github.com>
2024-09-11 23:45:09 +1100

    2.3.py

--- home_ex_2/2.3.py
+++ home_ex_2/2.3.py
@@ -1,9 +1,12 @@
 a = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 
 j = 0
-while a[j] >= 0 and j < len(a):
-    if a[j] == 0:
+while a[j] < len(a):
+    if a[j] > 0:
+        print(a[j])
+        j += 1
+    elif a[j] == 0:
         j += 1
         continue
-    print(a[j])
-    j += 1
+    else:
+        break
