9ddce95264095015207784b556382ce8174973e2
MoDerFEe <159480211+MoDerFEe@users.noreply.github.com>
2025-01-25 21:24:45 +1100

    Add files via upload

---
+++ home_ex_13/13.2.py
@@ -0,0 +1,23 @@
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message
+from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.filters import Command
+import asyncio
+
+api = '7659378126:AAFWQP38bbhJiVpRvOFhstqHflPrrYm4SWM'
+bot = Bot(token=api)
+dp = Dispatcher(storage=MemoryStorage())
+
+@dp.message(Command('start'))
+async def start_command_handler(message: Message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
+
+@dp.message()
+async def start_command_handler(message: Message):
+    await message.answer('Введите команду /start, чтобы начать общение.')
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
